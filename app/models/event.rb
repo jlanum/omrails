32fe7d04ccc59344db133cda727beeead9b7cf7f
Jesse Lanum <jesse.lanum@gmail.com>
2013-04-12 11:03:06 -0700

    Adding Length Validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
   attr_accessible :title, :starts_at, :ends_at, :all_day, :description
   
+  validates_length_of :description, :minimum => 3, :maximum => 60, :allow_blank => false
+  
   scope :between, lambda {|start_time, end_time|
     {:conditions => ["? < starts_at < ?", Event.format_date(start_time), Event.format_date(end_time)] }
   }
