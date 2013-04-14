320276546cb526e17aff79077353378985e789c3
Jesse Lanum <jesse.lanum@gmail.com>
2013-04-14 16:53:42 -0700

    Moving User Permission To View

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :brand, :price, :markdown, :markprice, :validate_max_pins_have_been_reached
+  attr_accessible :description, :brand, :price, :markdown, :markprice
   
   
   validates :description, presence: true
@@ -8,14 +8,14 @@ class Pin < ActiveRecord::Base
   validates :price, presence: true
   validates_length_of :description, :minimum => 3, :maximum => 25, :allow_blank => false
   validates :price, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {:greater_than => 0}
-  validate :validate_max_pins_have_been_reached
+  
   
   belongs_to :user
   
   has_attached_file :image, styles: { medium: "320x240>"}
-
-  def validate_max_pins_have_been_reached
-      if User.current != user.role? || user.pins.size <= 5 
+  
+    def validate_max_pins_have_not_been_reached
+      if User.current != user.role? || user.pins.size <= 5
       errors[:base] << "To track more products, please upgrade your account"
     end
   end
